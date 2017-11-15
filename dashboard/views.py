3bca6319b32b3048b5c30ca5a13495a6c6cf4f48
Masamichi Tanaka <masa25michi@hotmail.com>
2017-11-15 14:51:10 +0800

    very simple ajax

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
+
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 def index(request):
+    # get courses from database
     test_courses = ['COMP3335', 'COMP3421']
     return render(request, 'dashboard/board/index.html', {'courses': test_courses})
 
 def coursedetail(request, course_id):
     return render(request, 'dashboard/board/coursedetail.html', {'course_id': course_id})
+
+def getmessage(request):
+    course_id = request.POST['course_id']
+    message = request.POST['message']
+
+    messages = ['a', 'hi']
+
+    # insert into database
+    if message:
+        messages.append(message)
+
+    return JsonResponse(messages, safe=False)
