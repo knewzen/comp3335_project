7a4ba7d30969588b178b60f87dd51006633964b7
Masamichi Tanaka <masa25michi@hotmail.com>
2017-11-17 09:14:19 +0800

    fix bug::cannot show course-detail page

--- comp3335/urls.py
+++ comp3335/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^dashboard/', include('comp3335.dashboard.urls')),
+    url(r'^$', include('comp3335.dashboard.urls')),
 ]
