b60fea73dbd2c7448311409aa7aace5a6f90df31
Masamichi Tanaka <masa25michi@hotmail.com>
2017-11-20 17:24:12 +0800

    change dir of welcome templates

--- comp3335/welcome/urls.py
+++ comp3335/welcome/urls.py
@@ -4,5 +4,5 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='index.html'))
+    url(r'^$', TemplateView.as_view(template_name='welcome/index.html'))
 ]
