33789dc5c5103bc2f29f8f353b4e2a68e22c6377
Edward Fu <edwardfu850808@gmail.com>
2017-11-27 07:25:47 -0800

    added course to admin

--- comp3335/account/models.py
+++ comp3335/account/models.py
@@ -1,10 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Account(models.Model):
     email = models.EmailField(unique=True, null=False) #noEncrypt
     pwd_hash = models.CharField(max_length=255, null=False) #noEncrypt
-    salt = models.CharField(max_length=255, null=False) #noEncrypt
+    salt1 = models.CharField(max_length=255, null=False) #noEncrypt
+    salt2 = models.CharField(max_length=255, null=False) #noEncrypt
     f_name = models.CharField(max_length=90, null=False)
     l_name = models.CharField(max_length=90, null=False)
     age = models.CharField(max_length=64,null=False)
+
+#@receiver(post_save, sender=User)
+#def create_user_profile(sender, instance, created, **kwargs):
+#    if created:
+#        Profile.objects.create(user=instance)
+
+#@receiver(post_save, sender=User)
+#def save_user_profile(sender, instance, **kwargs):
+#    instance.profile.save()
