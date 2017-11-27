ee83ca2506242b8a9beaed6b9b9019e98365c0cd
IvanWeiYR <wyrivanngai@gmail.com>
2017-11-27 04:58:19 -0800

    combined

--- comp3335/account/models.py
+++ comp3335/account/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
+#from djangoaudit.models import AuditedModel
+
 # Create your models here.
 class Account(models.Model):
+    log_fields = ('email','pwd_hash','salt','f_name','l_name','age')
+
     email = models.EmailField(unique=True, null=False)
     pwd_hash = models.CharField(max_length=255, null=False)
     salt = models.CharField(max_length=255, null=False)
     f_name = models.CharField(max_length=30, null=False)
     l_name = models.CharField(max_length=30, null=False)
     age = models.IntegerField(null=False)
+
+    def __unicode__(self):
+        return self.email
